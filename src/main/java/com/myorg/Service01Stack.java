1fe34e6ce7ea2e2ef0a4272574aee804c389ffb4
Kevin Viana Pereira <kevin.pereira@wmtrading.com.br>
2022-11-21 14:47:02 -0300

    corrigindo subnets do RDS e nome do db

---
+++ src/main/java/com/myorg/Service01Stack.java
@@ -0,0 +1,83 @@
+package com.myorg;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.CpuUtilizationScalingProps;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.ScalableTaskCount;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+
+public class Service01Stack extends Stack {
+        public Service01Stack(final Construct scope, final String id, Cluster cluster) {
+                this(scope, id, null, cluster);
+        }
+
+        public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+                super(scope, id, props);
+
+                Map<String, String> environment = new HashMap<>();
+                environment.put("SPRING_DATASOURCE", "jdbc:mysql://"
+                                + Fn.importValue("rds-endpoint")
+                                + ":3306/aws-project01-db?createDatabaseIfNotExist=true&serverTimezone=UTC");
+                environment.put("SPRING_DATASOURCE_USERNAME", "admin");
+                environment.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("rds-password"));
+
+                ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder
+                                .create(this, "ALB01")
+                                .serviceName("service-01")
+                                .cluster(cluster)
+                                .cpu(256)
+                                .memoryLimitMiB(512)
+                                .desiredCount(2)
+                                .listenerPort(8080)
+                                .taskImageOptions(
+                                                ApplicationLoadBalancedTaskImageOptions.builder()
+                                                                .containerName("aws-project-01")
+                                                                .image(ContainerImage.fromRegistry(
+                                                                                "public.ecr.aws/l0t9d3q4/curso-aws-spring"))
+                                                                .containerPort(8080)
+                                                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                                                                .logGroup(LogGroup.Builder.create(this,
+                                                                                                "Service01LogGroup")
+                                                                                                .logGroupName("service-01")
+                                                                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                                                                .build())
+                                                                                .streamPrefix("service-01")
+                                                                                .build()))
+                                                                .environment(environment)
+                                                                .build())
+                                .publicLoadBalancer(true)
+                                .assignPublicIp(true)
+                                .build();
+
+                service01.getTargetGroup().configureHealthCheck(new HealthCheck.Builder()
+                                .path("/actuator/health")
+                                .port("8080")
+                                .healthyHttpCodes("200")
+                                .build());
+
+                ScalableTaskCount scalableTaskCount = service01
+                                .getService()
+                                .autoScaleTaskCount(EnableScalingProps.builder()
+                                                .minCapacity(2)
+                                                .maxCapacity(4)
+                                                .build());
+
+                scalableTaskCount.scaleOnCpuUtilization("Service01AutoScaling", CpuUtilizationScalingProps.builder()
+                                .targetUtilizationPercent(50)
+                                .build());
+        }
+}
---
+++ src/main/java/com/myorg/VpcStack.java
@@ -0,0 +1,29 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+
+public class VpcStack extends Stack {
+
+    private Vpc vpc;
+
+    public VpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public VpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        vpc = Vpc.Builder.create(this, id)
+                .vpcName("Vpc01")
+                .maxAzs(2)
+                .natGateways(0)
+                .build();
+    }
+
+    public Vpc getVpc() {
+        return vpc;
+    }
+}

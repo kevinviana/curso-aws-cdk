f9d3cd0addc3127522bed1686007f63953807b66
Kevin Viana Pereira <kevin.pereira@wmtrading.com.br>
2022-12-28 10:33:32 -0300

    remove unused imports

--- src/main/java/com/myorg/ClusterStack.java
+++ src/main/java/com/myorg/ClusterStack.java
@@ -3,8 +3,6 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ecs.Cluster;
 

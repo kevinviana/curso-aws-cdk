6abaeb200cbd6b85b5cbfe5ffd8fce00978189c1
Kevin Viana Pereira <kevin.pereira@wmtrading.com.br>
2022-12-28 10:34:54 -0300

    intanciando servico 2

--- src/main/java/com/myorg/CdkCloudformationApp.java
+++ src/main/java/com/myorg/CdkCloudformationApp.java
@@ -23,6 +23,11 @@ public class CdkCloudformationApp {
         service01Stack.addDependency(rdsStack);
         service01Stack.addDependency(snsStack);
 
+      Service02Stack service02Stack = new Service02Stack(app, "Service02", clusterStack.getCluster(),
+                snsStack.getProductEventsTopic());
+        service02Stack.addDependency(snsStack);
+        service02Stack.addDependency(clusterStack);
+        
         app.synth();
     }
 }
